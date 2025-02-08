589ef653b57e805430b3ddc4dc24bd47a95c8449
Shivabhandari122 <qshiva906@gmail.com>
2025-02-08 22:06:36 +0545

    freeze method used

--- day3.js
+++ day3.js
@@ -4,40 +4,39 @@
 3) Arrow function
 4) Annonymous function */
 
-// **********regular function********** 
+// **********regular function**********
 
-function add(a, b){
-    console.log(a + b)
+function add(a, b) {
+  console.log(a + b);
 }
-add(3, 5)
+add(3, 5);
 
 // ************Named function************
 
 // If you put our function name then it is called named function.
 
-let sum = function(a){
-    console.log(a)
-}
-sum(4)
+let sum = function (a) {
+  console.log(a);
+};
+sum(4);
 
 // *************Arrow function***********
 
-let a = () => console.log("hello i am shiva")           //This way used only if one line code in block.
-a()
+let a = () => console.log("hello i am shiva"); //This way used only if one line code in block.
+a();
 
-let sub = (a, b) =>{
-    console.log(a - b)
-    console.log("******************************************************")       //this arrrow function is used for multiline
-}
+let sub = (a, b) => {
+  console.log(a - b);
+  console.log("******************************************************"); //this arrrow function is used for multiline
+};
 sub(5, 2);
 
 // ************Annonymous function**************
 
-// some function have not named is known as anonymous function 
-
+// some function have not named is known as anonymous function
 
 (() => {
-    console.log("hello world!");
+  console.log("hello world!");
 })();
 
 // ***************Loops in javascript********************
@@ -46,50 +45,59 @@ sub(5, 2);
 
 //syntax:- for(intialization; condition; iteration) // i++ === i+1
 
-for(let i=0; i<5; i++){
-    console.log(i)
+for (let i = 0; i < 5; i++) {
+  console.log(i);
 }
-console.log("*****************************************")
+console.log("*****************************************");
 
-
-const people = ['shiva', 'jitesh', 'nitesh', 'kundan', 'adesh', 'kumar']
-for(let j=0; j < people.length; j++){
-    console.log(people[j])
+const people = ["shiva", "jitesh", "nitesh", "kundan", "adesh", "kumar"];
+for (let j = 0; j < people.length; j++) {
+  console.log(people[j]);
 }
 
 // *While loop*
 
 let z = 0;
-while(z < 5){
-    console.log(z)
-    z++
+while (z < 5) {
+  console.log(z);
+  z++;
 }
-console.log("**************************************************")
+console.log("**************************************************");
 
 // *Loop alternative for object variable*
 
 let company = {
-    employee : 'shiva bhandari',
-    age : 24,
-    salary: 25000
-}
+  employee: "shiva bhandari",
+  age: 24,
+  salary: 25000,
+};
 
-for(let all in company){
-    console.log(all /*this show just key*/ , company[all] /*this will show all the value*/)
+for (let all in company) {
+  console.log(
+    all /*this show just key*/,
+    company[all] /*this will show all the value*/
+  );
 }
-console.log("***************************************************************************")
+console.log(
+  "***************************************************************************"
+);
 
 // *************Hoisting***************************
 
 //Behaviour where variable and function declearation are moved to the top during compilation phase.
 
-//note it is working only in var variable and regular function only 
-sql()
-function sql(){
-    console.log("This is working")
+//note it is working only in var variable and regular function only
+sql();
+function sql() {
+  console.log("This is working");
 }
 
-console.log(action)
-var action = 1
+console.log(action);
+var action = 1;
 
-//this things are just working not show actual data 
+//this things are just working not show actual data
+
+const ab = () => {
+  const car = "tata";
+};
+Object.freeze(ab);
